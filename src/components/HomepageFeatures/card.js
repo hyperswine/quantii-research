4ac71f8aae52bfdcd1c464d94b78a2188b013a48
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2022-12-17 08:14:24 +1100

    Updates

--- src/components/HomepageFeatures/card.js
+++ src/components/HomepageFeatures/card.js
@@ -1,15 +1,20 @@
-// import chakra
-// and framer motion?
+import Card from 'react-bootstrap/Card'
+import { motion } from "framer-motion"
 
-{/* <Stack spacing='4'>
-  {['sm', 'md', 'lg'].map((size) => (
-    <Card key={size} size={size}>
-      <CardHeader>
-        <Heading size='md'> {size}</Heading>
-      </CardHeader>
-      <CardBody>
-        <Text>size = {size}</Text>
-      </CardBody>
+export default function InfoCard(title, text, body) {
+    <Card style={{ padding: '1rem', display: "flex", justifyContent: "space-between" }}>
+        <motion.div layout
+            whileHover={{ scale: 1.025, boxShadow: '2px 2px #9E9E9E' }}
+            whileTap={{ scale: 1.025 }}
+            style={{ marginBottom: '1rem' }}
+        >
+            <Card style={{ padding: '1rem' }}>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{text}</Card.Text>
+                    {body}
+                </Card.Body>
+            </Card>
+        </motion.div>
     </Card>
-  ))}
-</Stack> */}
+}
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,10 +2,24 @@ import React from "react"
 import clsx from "clsx"
 import styles from "./styles.module.css"
 import Quantii from "/static/img/Quantii.png"
+import InfoCard from "./card"
 
-function Feature({ Img, title, description }) {
+const FeaturedArticlesList = [
+  {
+    title: <a href="../docs/base/kernel">Kernels</a>,
+    Img: null,
+    description: <>Kernels: Utility and Foundation</>,
+  },
+  {
+    title: <a href="../blog/performance-post">Performance</a>,
+    Img: null,
+    description: <>[Blog] Performance: Part 1</>,
+  }
+]
+
+function FeaturedArticle({ Img, title, description, main_link }) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx("col col--4")} style={{ borderStyle: "solid", margin: "1rem" }}>
       <div className="text--center">
         <img src={Img} />
       </div>
@@ -20,13 +34,16 @@ function Feature({ Img, title, description }) {
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="column">
-          <img src={Quantii} />
-          Quantii, a step towards the future.
+      <div className="container" style={{ padding: '1rem', display: "flex", flexDirection: "column", alignItems: "centre" }}>
+        <div className="column" style={{ padding: '1rem', display: "flex", justifyContent: "center", marginBottom: "5rem" }}>
+          <img src={Quantii} style={{ marginRight: '2rem' }} />
+          <text style={{ alignSelf: "center" }}>Quantii, a step towards the future.</text>
         </div>
-        <div>
-          Articles:
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h2 className="text--center">Featured Articles</h2>
+          <div style={{ display: "flex", flexDirection: "row", margin: "2rem" }}>
+            {FeaturedArticlesList.map((props, idx) => (<FeaturedArticle key={idx} {...props} />))}
+          </div>
         </div>
       </div>
     </section>
